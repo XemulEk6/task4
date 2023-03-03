bed4d96468b3a18c4b16eb40f1ebcb8079d77a05
XemulEk6 <xemul.ek6@gmail.com>
2023-03-03 18:08:04 +0500

    endtask

--- Program.cs
+++ Program.cs
@@ -3,3 +3,23 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count <= userNumber)
+{
+
+    if (count % 2 != 1)
+     {
+         System.Console.Write(count);
+    count = count +1;
+    }
+
+    else
+    {
+        if (count % 2 == 1)
+        count = count + 1;
+    }
+}
